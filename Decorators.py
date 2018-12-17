6ed77c8289f1d49a320dcba7655adafc3af544b3
Rishant Dutt <rishant.dutt96@gmail.com>
2018-12-17 10:28:43 +0530

    Decorators, type-checking added.

--- Decorators.py
+++ Decorators.py
@@ -61,6 +61,51 @@ def repeat(number=3):
             result = None
             for _ in range(number):
                 result = function(*args, **kwargs)
-                return result
-            return wrapper
-        return actual_decorator
+            return result
+        return wrapper
+    return actual_decorator
+
+# Argument Checking: XML-RPC
+
+
+rpc_info = {}
+
+
+def xmlrpc(in_=(), out=(type(None),)):
+    def _xmlrpc(function):
+        # registering the signature
+        func_name = function.__name__
+        rpc_info[func_name] = (in_, out)
+
+        def _check_types(elements, types):
+            """Subfunction that checks the types."""
+            if len(elements) != len(types):
+                raise TypeError('argument count is wrong')
+            typed = enumerate(zip(elements, types))
+            for index, couple in typed:
+                arg, of_the_right_type = couple
+                if isinstance(arg, of_the_right_type):
+                    continue
+                raise TypeError(
+                    'arg #%d should be of %s' % (index,
+                                                 of_the_right_type))
+
+        # wrapped function
+        def __xmlrpc(*args):  # no kewywords alloewd
+            # checking what goes in
+            checkable_args = args[1:]  # removing self
+            _check_types(checkable_args, in_)
+            # running the function
+            res = function(*args)
+            # checking what goes out
+            if not type(res) in (tuple, list):
+                checkable_res = (res,)
+            else:
+                checkable_res = res
+            _check_types(checkable_res, out)
+
+            # the function and the type
+            # checking succeeded
+            return res
+        return __xmlrpc
+    return _xmlrpc
